515fa1648f60d8221a95409873d6b3e71ad93886
Ashutosh Mishra <68610225+ashupn10@users.noreply.github.com>
2024-07-27 18:26:58 +0000

    Pending changes exported from your codespace

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,17 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
+        
         System.out.println("Hello Remote World!");
+        Calculator calc = new Calculator();
+        int result = calc.add(5,6);
+        System.out.println(result);
     }
-}
+};
+
+class Calculator{
+    public int add(int a, int b){
+        System.out.println("in add");
+        return a+b;
+    }
+};
